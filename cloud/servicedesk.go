d78007f80e1eef6e8f05c890249d6a21499ec3a1
Zate Berg <zberg@atlassian.com>
2024-04-23 15:13:36 +0800

    adding service desk items.

--- cloud/servicedesk.go
+++ cloud/servicedesk.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/http/httputil"
 
 	"github.com/google/go-querystring/query"
 )
@@ -248,4 +249,116 @@ func (s *ServiceDeskService) ServiceDeskinfo(ctx context.Context) (*ServiceDeskI
 	}
 
 	return serviceDeskInfo, resp, nil
+}
+
+// ServiceDeskListOptions specifies the optional parameters to the ServiceDeskService.ListserviceDesks method.
+type ServiceDeskListOptions struct {
+	Start int `url:"start,omitempty"`
+	Limit int `url:"limit,omitempty"`
+}
+
+// ServiceDeskList represents a list of service desks in the Jira Service Management instance.
+type ServiceDeskList struct {
+	Expands    string `json:"_expands,omitempty"`
+	Size       int    `json:"size,omitempty"`
+	Start      int    `json:"start,omitempty"`
+	Limit      int    `json:"limit,omitempty"`
+	IsLastPage bool   `json:"isLastPage,omitempty"`
+	Links      struct {
+		Base    string `json:"base,omitempty"`
+		Context string `json:"context,omitempty"`
+		Next    string `json:"next,omitempty"`
+		Prev    string `json:"prev,omitempty"`
+	} `json:"_links,omitempty"`
+	Values []struct {
+		ID          string `json:"id,omitempty"`
+		ProjectID   string `json:"projectId,omitempty"`
+		ProjectName string `json:"projectName,omitempty"`
+		ProjectKey  string `json:"projectKey,omitempty"`
+		Links       struct {
+			Self string `json:"self,omitempty"`
+		} `json:"_links,omitempty"`
+	} `json:"values,omitempty"`
+}
+
+// ListServiceDesks returns a list of all service desks in the Jira Service Management instance.
+//
+// https://developer.atlassian.com/cloud/jira/service-desk/rest/api-group-servicedesk/#api-rest-servicedeskapi-servicedesk-get
+func (s *ServiceDeskService) ListServiceDesks(ctx context.Context, options *ServiceDeskListOptions) (*ServiceDeskList, *Response, error) {
+	req, err := s.client.NewRequest(ctx, http.MethodGet, "rest/servicedeskapi/servicedesk", nil)
+	if err != nil {
+		return nil, nil, fmt.Errorf("could not create a new request: %w", err)
+	}
+
+	if options != nil {
+		q, err := query.Values(options)
+		if err != nil {
+			return nil, nil, fmt.Errorf("could not create query values: %w", err)
+		}
+		req.URL.RawQuery = q.Encode()
+	}
+
+	resp, err := s.client.Do(req, nil)
+	if err != nil {
+		reqDump, _ := httputil.DumpRequestOut(resp.Request, true)
+		if reqDump != nil {
+			fmt.Printf("Request Dump: %s\n", reqDump)
+		}
+
+		respDump, _ := httputil.DumpResponse(resp.Response, true)
+		if respDump != nil {
+			fmt.Printf("Response Dump: %s\n", respDump)
+		}
+
+		return nil, resp, NewJiraError(resp, fmt.Errorf("client.do failed in ListServiceDesks: %w", err))
+	}
+	defer resp.Body.Close()
+
+	if resp.Response.StatusCode != http.StatusOK {
+		return nil, resp, NewJiraError(resp, fmt.Errorf("could not get service desk info"))
+	}
+
+	serviceDeskList := new(ServiceDeskList)
+
+	if err := json.NewDecoder(resp.Body).Decode(serviceDeskList); err != nil {
+		return nil, resp, fmt.Errorf("could not unmarshall the data into struct")
+	}
+
+	return serviceDeskList, resp, nil
+
+}
+
+// ServiceDesk is a struct that represents a JIRA Service Desk
+type ServiceDesk struct {
+	ID          string `json:"id,omitempty"`
+	ProjectID   string `json:"projectId,omitempty"`
+	ProjectName string `json:"projectName,omitempty"`
+	ProjectKey  string `json:"projectKey,omitempty"`
+	Links       struct {
+		Self string `json:"self,omitempty"`
+	} `json:"_links,omitempty"`
+}
+
+// GetServiceDeskByID returns a service desk by its ID.
+//
+// https://developer.atlassian.com/cloud/jira/service-desk/rest/api-group-servicedesk/#api-rest-servicedeskapi-servicedesk-servicedeskid-get
+func (s *ServiceDeskService) GetServiceDeskByID(ctx context.Context, serviceDeskID interface{}) (*ServiceDesk, *Response, error) {
+	apiEndpoint := fmt.Sprintf("rest/servicedeskapi/servicedesk/%v", serviceDeskID)
+	req, err := s.client.NewRequest(ctx, http.MethodGet, apiEndpoint, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	resp, err := s.client.Do(req, nil)
+	if err != nil {
+		return nil, resp, NewJiraError(resp, err)
+	}
+	defer resp.Body.Close()
+
+	serviceDesk := new(ServiceDesk)
+	if err := json.NewDecoder(resp.Body).Decode(serviceDesk); err != nil {
+		return nil, resp, fmt.Errorf("could not unmarshall the data into struct")
+	}
+
+	return serviceDesk, resp, nil
 }
